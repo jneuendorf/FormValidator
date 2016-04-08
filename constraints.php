e6e3d5fdef2b15d8ea928e2d03b218f33acf96c0
Jim Neuendorf <jim.neuendorf@gmx.de>
2016-04-08 09:19:31 +0200

    adjusted constraints tab

--- constraints.php
+++ constraints.php
@@ -1,40 +1,81 @@
-The following validators come with the FormValidator. The list shows what values will be interpreted as <i>valid</i>.
-<ul class="spaced">
-    <li>
-        <code>checkbox</code><br>
-        a checked checkbox
-    </li>
-    <li>
-        <code>email</code><br>
-        &lt;ANYTHING+&gt;@&lt;ANYTHING_WITH_A_DOT+&gt;
-    </li>
-    <li>
-        <code>integer</code><br>
-        any integer <var class="math">x</var> &#8712; &#8484;,<br>
-        Range can be defined with <code>data-fv-min</code> &amp; <code>data-fv-max</code> (and <code>data-fv-include-min</code> &amp; <code>data-fv-include-max</code> but that wouldn't make so much sense).
-    </li>
-    <li>
-        <code>number</code><br>
-        any number <var class="math">x</var> &#8712; &#8477; (that JavaScript can represent),<br>
-        Range can be defined with <code>data-fv-min</code> &amp; <code>data-fv-max</code> and <code>data-fv-include-min</code> &amp; <code>data-fv-include-max</code>.
-    </li>
-    <li>
-        <code>phone</code><br>
-        any combination of <code>"0123456789+-/() "</code> (that's longer than 2 characters by default),<br>
-        As with <code>text</code> a minimum and maximum length can be defined using <code>data-fv-min</code> and <code>data-fv-max</code>.
-    </li>
-    <li>
-        <code>radio</code><br>
-        a radio button of the element's group of radio buttons (= all with the same <code>name</code> attribute) must be checked
-    </li>
-    <li>
-        <code>select</code><br>
-        the selected item's value is not <code>null</code> and at least 1 character long,<br>
-        As with <code>text</code> a minimum and maximum length can be defined using <code>data-fv-min</code> and <code>data-fv-max</code>.
-    </li>
-    <li>
-        <code>text</code><br>
-        anything longer than 0 characters,<br>
-        A minimum and maximum length can be defined using <code>data-fv-min</code> and <code>data-fv-max</code>.
-    </li>
-</ul>
+<h4>The following constraints come with the FormValidator</h4>
+<dl>
+    <dt><code>data-fv-blacklist</code></dt>
+    <dd>
+        <code>String</code><br>
+        Define a set of characters of which none may occur in the validation value of the field.<br>
+        There are no default options for this constraint.
+    </dd>
+
+    <dt><code>data-fv-max</code></dt>
+    <dd>
+        <code>Number</code> <span class="label label-option">data-fv-include-max</span><br>
+        Define the maximum value for an element.<br>
+        By default this value is the greatest valid value. To exclude the maximum see the option <code>data-fv-include-max</code>.
+    </dd>
+
+    <dt><code>data-fv-max-length</code></dt>
+    <dd>
+        <code>Number</code><br>
+        Define the maximum length for the validation value of an element.<br>
+        This value is the greatest valid value.<br>
+        There are no default options for this constraint.
+    </dd>
+
+    <dt><code>data-fv-min</code></dt>
+    <dd>
+        <code>Number</code> <span class="label label-option">data-fv-include-min</span><br>
+        Define the minimum value for an element.<br>
+        By default this is be the smallest valid value. To exclude the minimum see the option <code>data-fv-include-min</code>.
+    </dd>
+
+    <dt><code>data-fv-min-length</code></dt>
+    <dd>
+        <code>Number</code><br>
+        Define the minimum length for the validation value of an element.<br>
+        This value is the smallest valid value.<br>
+        There are no default options for this constraint.
+    </dd>
+
+    <dt><code>data-fv-regex</code></dt>
+    <dd>
+        <code>String</code> <span class="label label-option">data-fv-regex-flags</span><br>
+        Define a regular expression that the element's validation value must not match.<br>
+    </dd>
+
+    <dt><code>data-fv-whitelist</code></dt>
+    <dd>
+        <code>String</code><br>
+        Define a set of characters that the element's validation value must contain.<br>
+        There are no default options for this constraint.
+    </dd>
+</dl>
+
+
+<h4>The following options for constraints come with the FormValidator</h4>
+<dl>
+    <dt><code>data-fv-include-max</code></dt>
+    <dd>
+        <code>Boolean</code> <span class="label label-option">data-fv-max</span><br>
+        An option for <code>data-fv-max</code>.
+        Define whether to include the defined <code>data-fv-max</code> or not.
+    </dd>
+
+    <dt><code>data-fv-include-min</code></dt>
+    <dd>
+        <code>Boolean</code> <span class="label label-option">data-fv-min</span><br>
+        An option for <code>data-fv-min</code>.
+        Define whether to include the defined <code>data-fv-min</code> or not.
+    </dd>
+
+    <dt><code>data-fv-regex-flags</code></dt>
+    <dd>
+        <code>String</code> <span class="label label-option">data-fv-regex</span><br>
+        An option for <code>data-fv-regex</code>. Define a what flags to use for the defined regular expression.
+    </dd>
+</dl>
+
+
+<h4>Note</h4>
+If you define a constraint to a validator so that the combination does not make sense <code>FormValidator</code> won't throw or report anything nor try anything fancy. I decided to keep the code simple (and as lightweight as possible) and make the user take care of correctness of validation definitions.<br>
+<code>FormValidator</code> alwasy assumes syntactically and logically correct input from the programmer.
