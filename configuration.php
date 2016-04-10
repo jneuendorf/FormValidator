fc431ed54bff3a0a4044f1b061a58b2da7dfcfdc
Jim Neuendorf <jim.neuendorf@gmx.de>
2016-04-10 21:03:23 +0200

    working on configure() method

--- configuration.php
+++ configuration.php
@@ -1,7 +1,9 @@
 <h4>List of configuration constants</h4>
-The following configuration constants are attached to the <code>FormValidator</code> class.<br>
-If it has a <code>DEFAULT</code> property it points to the according constant marked with <span class="label label-option">default</span>.<br>
-Most of them simulate <code>enum</code> of other languages but some of them can be used as variables. Those are marked with <span class="label label-var">variable</span>.
+<p>
+    The following configuration constants are attached to the <code>FormValidator</code> class.<br>
+    If it has a <code>DEFAULT</code> property it points to the according constant marked with <span class="label label-option">default</span>.<br>
+    Most of them simulate <code>enum</code> of other languages but some of them can be used as variables. Those are marked with <span class="label label-var">variable</span>.
+</p>
 
 <dl>
     <dt><code>BUILD_MODES</code></dt>
@@ -129,3 +131,63 @@ Most of them simulate <code>enum</code> of other languages but some of them can 
         </ul>
     </dd>
 </dl>
+<br>
+<h4>Configuring internal parameters (advanced)</h4>
+<p>
+    If you want to change some of <code>FormValidator's</code> behavior without modifying its source code directly you can use the <code>FormValidator.configure</code> method:
+    <dl>
+        <dt><code>Function FormValidator.configure(Function callback) -> FormValidatorClass</code> <span class="label label-option">chainable</span></dt>
+        <dd>
+            The <code>callback</code> function parameter has the signature <code>Function callback(Object configurations)</code>.<br>
+            The <code>configurations</code> object is a set of internal variables that influence the behavior of the <code>FormValidator</code> and are made accessible only in that way.<br>
+            This is the list of internal variables:
+            <ul>
+                <li>
+                    <code>constraint_validator_groups (Array (of Array of String))</code><br>
+                    Defines which constraint validators are logically connected (and thus can potentially be combined to a new error message. For example, if you wanted to have a special error message in case the 2 constraint validators <code>blacklist</code> and <code>whitelist</code> are violated you would add an according group with<br>
+                    <code class="quote">constraint_validator_groups.push([&quot;blacklist&quot;, &quot;whitelist&quot;]);</code>
+                    Now you would add a new entry <code>constraint_blacklist_whitelist</code> in the <code>locales</code> variables and it will be used.<br>
+                    The groups defined in that array must be disjoint.
+                </li>
+                <li>
+                    <code>constraint_validator_options_in_locale_key</code><br>
+                    <!-- # define when to include a constraint validator option in the locale key (and when not - if not matching the below value)
+                    # value can also be: function(String locale) -> Mixed value
+                    # i.e. for include_max a function could be defined if different locales formulate the according error message differently:
+                    # - EN could be like 'the value is not less than {{max}}' (max is not included in valid range => locale key = "constraint_max") and
+                    # - DE could be like 'is not less than or equal to {{max}}' (max is included in valid range => locale key = "constraint_max_include_max")
+                    # => in that case the function would look like:
+                    # (locale) ->
+                    #   if locale is "en"
+                    #       return false
+                    #   if locale is "de"
+                    #       return true
+                    #   # all other locales...
+                    #   return something -->
+                </li>
+                <li>
+                    <code>constraint_validator_options</code><br>
+                    <!-- # define which constraint-validator options are compatible with a constraint validator
+                    # they are accessible in the according constraint validator in the options object (last parameter)
+                    # used in _validate_constraints()
+                    # => constraint validator options:
+                    #    - data-fv-include-max
+                    #    - data-fv-include-min
+                    #    - data-fv-regex-flags -->
+                </li>
+                <li>
+                    <code>locale_message_builders</code><br>
+
+                </li>
+                <li>
+                    <code>message_builders</code><br>
+
+                </li>
+                <li>
+                    <code>message_data_creators</code><br>
+
+                </li>
+            </ul>
+        </dd>
+    </dl>
+</p>
